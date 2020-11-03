2d303777b23d463ab4e3796f739fd704c9869912
Treasury Quants <69048801+treasuryquants@users.noreply.github.com>
2020-11-03 15:39:06 +0000

    Add files via upload

---
+++ python_files/TQ_example_price_fx_forward.py
@@ -0,0 +1,50 @@
+# TreasuryQuants.com Ltd.
+# email: contact@treasuryquants.com
+# Note: this software is provided "as-is" under the agreed terms of your account.
+#       For more information see https://treasuryquants.com/terms-of-services/
+from TQPython.python_files import TQConnection, TQRequests
+
+# API: price_vanilla_swap
+# Explanation: It prices a vanilla interest rate swap.
+# Arguments: Run this API with following arguments:
+#         asof (example '20201022')
+#         type (example 'ir_vanilla_swap')
+#         notional (example 100000000)
+#         trade_date (example '20201022')
+#         trade_expiry (example '20201022')
+#         pay_currency (example 'gbp')
+#         receive_amount (example 1000000)
+#         receive_currency (example 'usd')
+#
+# About this example: In this example, we use the arguments above to price an fx forward.
+
+
+
+connection = TQConnection.Connection()
+#
+# Check if we have connections
+#
+request_ip_return = TQRequests.request_ip_return()
+message = connection.send(request_ip_return)
+if not message.is_ok:
+    print(message.is_ok, message.content)
+    exit
+
+
+#
+# FX Forward entry
+#
+market_fx_forward = TQRequests.request_function_price_fx_forward(
+    asof=20201022
+    , type='fx_forward'
+    , trade_date=20201022
+    , trade_expiry=20201222
+    , pay_amount=1000000
+    , pay_currency='gbp'
+    , receive_amount=1000000
+    , receive_currency='usd'
+    # , save_as = 'test_fx_forward.xml' # save_as is optional for if you want to save your trade in your workspace. For example, for risking later on.
+)
+message = connection.send(market_fx_forward)
+print("result status:{} cost:{} balance:{} content:{}".format(message.is_ok,connection.cost,connection.balance, message.content))
+
---
+++ python_files/TQ_example_risk_ladder.py
@@ -0,0 +1,80 @@
+# TreasuryQuants.com Ltd.
+# email: contact@treasuryquants.com
+# Note: this software is provided "as-is" under the agreed terms of your account.
+#       For more information see https://treasuryquants.com/terms-of-services/
+from TQPython.python_files import TQConnection, TQRequests
+
+# API: risk_ladder
+# Explanation: It calculates ladder sensitivities of a saved trade for a given date.
+# Arguments: Run this API with two arguments: 1) asof date and 2) tradeId
+#
+# About this example: We break down the example below into three steps:
+#                    1) Price a trade with "save_as" optional argument. (see price_vanilla_swap example for more)
+#                       The result would be the price of the trade ensuring that there is no issues with the trade
+#                       arguments, etc.
+#                    2) Risk the trade using only the trade id.
+#                    3) Delete the trade to clean up this example. (see workspace example for more)
+
+
+connection = TQConnection.Connection()
+#
+# Check if we have connections
+#
+request_ip_return = TQRequests.request_ip_return()
+message = connection.send(request_ip_return)
+if not message.is_ok:
+    print(message.is_ok, message.content)
+    exit
+
+
+
+#
+# Step 1 - Price a trade with "save_as" optional argument.
+#
+tradeId='test_ir_swap'
+asof='20201022'
+
+market_swap_rates = TQRequests.request_function_price_vanilla_swap(
+        asof=asof
+        , type='ir_vanilla_swap'
+        , notional=100000000
+        , trade_date='20201022'
+        , trade_maturity='30Y'
+        , index_id='eur.libor6m'                  # See show_available api to see the various other options
+        , discount_id='eur.ois'                   # See show_available api to see the various other options
+        , floating_leg_period='6M'
+        , fixed_leg_period='1y'
+        , floating_leg_daycount='act/360'         # See show_available api to see the various other options
+        , fixed_leg_daycount='30/360'             # See show_available api to see the various other options
+        , spread=0
+        , fixed_rate=-0.000032181335              # You can take this number from the market_swap_rates API
+        , is_payer=False
+        , business_day_rule='modified_following'  # See show_available api to see the various other options
+        , business_centres='london,target'        # See show_available api to see the various other options
+        , spot_lag_days=2
+        , save_as = tradeId # "save_as" is optional for when you want to save your trade in your workspace. For example, for risking later on.
+)
+message = connection.send(market_swap_rates)
+print("result status:{} cost:{} balance:{} content:{}".format(message.is_ok,connection.cost,connection.balance, message.content))
+
+
+if not message.is_ok:
+        exit
+
+
+
+#
+# Step 2 - Risk the trade using only the trade id.
+#
+request_risk = TQRequests.request_function_risk_ladder(asof, tradeId)
+message = connection.send(request_risk)
+print("result status:{} cost:{} balance:{} content:{}".format(message.is_ok,connection.cost,connection.balance, message.content))
+
+
+
+#
+# Step 3 - Delete the trade to clean up this example.
+#
+function_workspace_delete_file = TQRequests.request_function_workspace_delete_file(tradeId)
+message = connection.send(function_workspace_delete_file)
+print("result status:{} cost:{} balance:{} content:{}".format(message.is_ok,connection.cost,connection.balance, message.content))
